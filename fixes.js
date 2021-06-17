7a9a66a39ed11f6218505906cbca0781732b0e26
Eduardo Vieira Lemes <edu.vlemes@gmail.com>
2021-06-17 18:40:06 -0300

    Update fixes.js

--- fixes.js
+++ fixes.js
@@ -1,1 +1,12 @@
-console.log('Fixes file loaded');
+let apx_analytics = document.createElement('script');
+apx_analytics.src = "https://www.googletagmanager.com/gtag/js?id=G-V0HB6YB66J"
+document.head.append(apx_analytics);
+apx_analytics.onload = function() {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-V0HB6YB66J');
+  console.log('Tracking - OK');
+};
+
+console.log('Settings and fixes file loaded');
