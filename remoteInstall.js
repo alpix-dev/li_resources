51b9930c8eef1e182db90d3572a4d978deb7cd02
eduvlemes <edu.vlemes@gmail.com>
2021-07-29 13:19:00 -0300

    4268 footer n product page

--- remoteInstall.js
+++ remoteInstall.js
@@ -22,7 +22,6 @@ let script2 = document.createElement('script');
 script2.src = "http://127.0.0.1:5500/4268/v1/apx_4268.js"
 document.head.append(script2);
 script2.onload = function() {
-    apx_widgets.worker.pixPaymentMethod.style = "<style>/* PIX FOOTER */.apx_widgets_worker-pixPaymentMethod-footer{}.apx_widgets_worker-pixPaymentMethod-footer .icon{height:26px;} .apx_widgets_worker-pixPaymentMethod-footer .icon > *{max-height:100%;width:auto;}</style>";
     apx_widgets.worker.pixPaymentMethod.list.push({target : "#pagamento520160 .forma-conteiner > img"});
     apx_widgets.worker.pixPaymentMethod.list.push({target : ".pagina-produto .text-parcelas + img[src^='https://cdn.awsli.com.br/production/static/img/formas-de-pagamento/boleto-logo.png']"});
     apx_widgets.worker.run();
