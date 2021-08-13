2672341e67e9ddbc3759f70c6c903df7eb43a6a3
eduvlemes <edu.vlemes@gmail.com>
2021-08-13 16:51:38 -0300

    4398_

--- 4398/v1/apx_4398.js
+++ 4398/v1/apx_4398.js
@@ -1,6 +1,6 @@
 apx_widgets.worker.shelfLife = {};
 apx_widgets.worker.shelfLife.list = [{}];
-apx_widgets.worker.shelfLife.config = {};
+apx_widgets.worker.shelfLife.config = {prefixo: "DV"};
 apx_widgets.worker.shelfLife.match = $('.pagina-produto').length > 0 ;
 apx_widgets.worker.shelfLife.run = function(el){
     $('.principal .acoes-produto').each(function(){
@@ -8,14 +8,12 @@ apx_widgets.worker.shelfLife.run = function(el){
         sku = sku.split('-');
         let dataValidade = sku[sku.length -1];
 
-        if(dataValidade.startsWith('DV')){
-            dataValidade = dataValidade.replace('DV','');
+        if(dataValidade.startsWith(apx_widgets.worker.shelfLife.config.prefixo)){
+            dataValidade = dataValidade.replace(apx_widgets.worker.shelfLife.config.prefixo,'');
             dataValidade = dataValidade.slice(0,2) + '/' + dataValidade.slice(2,4) + '/' + dataValidade.slice(4,8);
 
             $('<label>Validade: '+ dataValidade+'</label>').prependTo($(this).find('div.comprar'));
         }
-        console.log(dataValidade)
-
     })
     console.log('apx_widgets.worker.shelfLife OK');
 };
