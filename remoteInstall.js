ede8f50d45c0a329d3140beca53669f3fcb249db
eduvlemes <edu.vlemes@gmail.com>
2021-08-21 00:07:20 -0300

    4473 stripe

--- remoteInstall.js
+++ remoteInstall.js
@@ -62,3 +62,34 @@ script2.onload = function() {
     apx_widgets.worker.run();
     console.log('script loaded');        
 };
+
+
+
+const apx_widgets = {}
+apx_widgets.functions = [];
+apx_widgets.scripts = [];
+
+apx_widgets.worker = {};
+apx_widgets.worker.run = function(){
+    
+    $.each(apx_widgets.worker, function(k, item){
+        if(k != "run"){
+            if(apx_widgets.worker[k].style !== undefined){
+                $('head').append(apx_widgets.worker[k].style);
+            }
+            if(apx_widgets.worker[k].list.length > 0 && apx_widgets.worker[k].match){
+                apx_widgets.worker[k].run();
+            }            
+        }        
+    })
+    $('[alt="Logomarca Loja Integrada"]').closest('a').attr('href','https://app.lojaintegrada.com.br/public/assinar?utm_source=partners&utm_campaign=399');
+    
+};
+
+let script2 = document.createElement('script');
+script2.src = "http://127.0.0.1:5501/4240/v1/apx_4240.js"
+document.head.append(script2);
+script2.onload = function() {
+    apx_widgets.worker.run();
+    console.log('script loaded');        
+};
